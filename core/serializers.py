81eb3de1a6e5f4bd2f60708f87266094b4a19889
speedwwr <gui.maciel.correa20042gmail.com>
2025-05-19 20:21:12 -0400

    cd

--- core/serializers.py
+++ core/serializers.py
@@ -41,18 +41,3 @@ class UserSerializer(serializers.ModelSerializer):
         model = models.User
         fields = '__all__'
 
-class UserSignupSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
-    email = serializers.EmailField(required=True)
-
-    class Meta:
-        model = User
-        fields = '__all__'
-
-    def create(self, validated_data):
-        user = User.objects.create_user(
-            username=validated_data['username'],
-            email=validated_data['email'],
-            password=validated_data['password']
-        )
-        return user
