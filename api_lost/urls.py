5201f34ccdbb1a6161d21a67d2d6b77bc773f89d
speedwwr <gui.maciel.correa20042gmail.com>
2025-05-19 20:28:01 -0400

    cd

--- api_lost/urls.py
+++ api_lost/urls.py
@@ -18,8 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-from core.views import SignupView, LoginAPIView
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
