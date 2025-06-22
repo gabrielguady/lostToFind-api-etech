203bb580be14f1e5383b17f248811c2a05a49dc4
gabrielguady <gabrielguady@gmail.com>
2025-06-22 15:43:24 -0400

    aws

--- api_lost/asgi.py
+++ api_lost/asgi.py
@@ -13,7 +13,6 @@ from django.core.asgi import get_asgi_application
 
 import os
 import django
-from channels.routing import get_default_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "api_lost.settings")
 django.setup()
